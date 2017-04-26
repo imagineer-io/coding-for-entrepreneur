8be6df1599c78bac616aed4de372da81f867b28e
Marco <marco@Minsoos-MacBook-Air.local>
2017-04-26 17:50:11 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by marco on 4/26/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("hello");
+
+        // operators
+        Operators operators = new Operators();
+        operators.calculate();
+    }
+}
